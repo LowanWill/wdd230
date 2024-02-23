e86f7eaaf1ef8148938cfd4a58577085ba54a57d
LowanWill <lowannwil@byui.edu>
2024-02-22 21:14:53 -0700

    week 7 lazy load demo

---
+++ lesson07/scripts/last-modified.js
@@ -0,0 +1,7 @@
+const d = new Date();
+const lastModified = document.querySelector("#lastModified");
+lastModified.innerHTML = `Last Modified: ${new Date().toLocaleString("default", "long")}`;
+const copyright = document.querySelector("#copyright");
+const year = d.getFullYear();
+copyright.innerHTML = `© ${new Date().getFullYear()}`;
+
