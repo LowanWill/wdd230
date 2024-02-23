c41b460b642c4c3aa296596a04e3a442801e1195
LowanWill <lowannwil@byui.edu>
2024-02-22 22:30:56 -0700

    Adding page visit counter

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -13,3 +13,18 @@ hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
 });
+
+//page visits
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
