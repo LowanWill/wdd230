52ae1e303e06ea3a65bba87234c15b5132a5e7be
LowanWill <lowannwil@byui.edu>
2024-05-09 21:40:49 -0600

    learning activities week 3 and update home page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,12 @@ lastModified.innerHTML = `Last Modified: ${new Date().toLocaleString("default", 
 const copyright = document.querySelector("#copyright");
 const year = d.getFullYear();
 copyright.innerHTML = `© ${new Date().getFullYear()}`;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
